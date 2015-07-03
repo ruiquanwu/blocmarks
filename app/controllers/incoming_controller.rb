4b210582fa8d1219ac100a3f8f6114e0429b85f8
ruiquan wu <ruiquanwu@gmail.com>
2015-07-03 21:52:47 +0000

    try incoming email in heroku

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -12,16 +12,29 @@ class IncomingController < ApplicationController
     # magic here. 
     
     # Find the user by using params[:sender]
+    sender = params[:sender]
      # Find the topic by using params[:subject]
+    subject = params[:subject]
      # Assign the url to a variable after retreiving it from params["body-plain"]
+    url = params["body-plain"]
 
      # Check if user is nil, if so, create and save a new user
-
+    @user = User.where(email: sender).first
+    unless @user
+      @user = User.new(email: sender, password: "helloworld")
+      @user.skip_confirmation!
+      @user.save
+    end
      # Check if the topic is nil, if so, create and save a new topic
-
+    @topic = Topic.where(title: subject).first
+    if @topic
+      
+    else
+      @topic = Topic.new(title: subject, user:@user)
+    end
      # Now that you're sure you have a valid user and topic, build and save a new bookmark
-
-    
+    @bookmark = Bookmark.new(url: url, topic: @topic)
+    @bookmark.save
     # Assuming all went well. 
     head 200
   end
